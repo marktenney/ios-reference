0b0b22940a897f069eecb2540b295dc0f657a89b
Mark Tenney <mark@dgtl.church>
2021-04-23 17:06:40 -0400

    update

--- reference.swift
+++ reference.swift
@@ -7,33 +7,45 @@ let double: Double = 3.1415926
 let checkYesOrNo: Bool = true
 let textString: String = "Hello, world!"
 
+// Defining Arrays
 let array: Array = [
     "String value", true, 3.14 
 ]
 
+// Defining Dictionaries
 let dictionary: Dictionary = [
     key: value,
     "Answer": "In the form of a question?"
 ]
 
+// Defining Variables & Constants
 var myVariable = 42
 let myConstant = 12
+
+// Setting a specific type of Variable or Constant
 var typedVariable: String
 let typedConstant: String
 
+// Creating a function
 func myFunctionName(input: Type) ->Type {
     // Do Something
     return theOutput
 }
 
+// Calling a function
+myFunctionName()
+
+// Creating a structure
 struct MyStruct {
 
 }
 
+// Creating a class
 class MyClass: SuperClass {
 
 }
 
+// Conditional statements
 if condition {
     // do X
 } elseif otherCondition {
@@ -42,6 +54,7 @@ if condition {
     // do Z
 }
 
+// Switch statements
 switch someVariable {
     case 1:
         // do X
@@ -51,6 +64,7 @@ switch someVariable {
         // do X
 }
 
+// Loops
 for variable in low...high {
     //loop
 }
